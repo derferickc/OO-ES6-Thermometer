885b90f998d54b2864b5a47e45f9435a717adc84
Frederick Choe <frederick@glamhive.com>
2021-01-12 01:52:31 -0800

    threshold assigner created

--- WotC_thermometer.js
+++ WotC_thermometer.js
@@ -2,9 +2,9 @@ class Thermometer {
 	constructor (_celcius) {
 		this.celcius = _celcius
 		this.farenheit = (_celcius * 9) / 5 + 32
-		this.freezing = 0
-		this.boiling = 100
-		this.threshold = null
+		this.thresholdName = null
+		this.thresholdTemperature = null
+		this.thresholdDirection = null
 		this.alertThrown = false
 	}
 
@@ -12,56 +12,82 @@ class Thermometer {
 		return `Celcius: ${this.celcius}°, Farenheit: ${this.farenheit}°`
 	}
 
-	setThreshold (_threshold) {
-		const lowercaseThreshold = _threshold.toLowerCase()
-		if (this.threshold !== _threshold) {
-			this.alertThrown = false
-			if (_threshold === 'boiling') {
-				this.threshold = _threshold
-			} else if (_threshold === 'freezing') {
-				this.threshold = _threshold
-			} else {
-				return 'this threshold is not accounted for!'
-			}
-		}
+	setThreshold (_thresholdName, _thresholdTemperature, _thresholdDirection) {
+		this.thresholdName = _thresholdName
+		this.thresholdTemperature = _thresholdTemperature
+		this.thresholdDirection = _thresholdDirection
+		this.alertThrown = false
 	}
 
 	setTemperature (_celcius) {
+		let temperatureDirection = ''
+		let temperatureChange = this.celcius - _celcius
+		if (temperatureChange > 0) {
+			temperatureDirection = 'decrease'
+		} else if (temperatureChange < 0) {
+			temperatureDirection = 'increase'
+		} else {
+			temperatureDirection = 'no change'
+		}
+		let previousCelcius = this.celcius
 		this.celcius = _celcius
 		this.farenheit = (_celcius * 9) / 5 + 32
-		let thresholdReachedCheck = this.thresholdReachedCheck()
+
+		let thresholdReachedCheck = this.thresholdReachedCheck(temperatureDirection, previousCelcius)
 		if (thresholdReachedCheck === true) {
-			if (this.threshold === 'boiling' && this.alertThrown === false) {
-				this.alertThrown = true
-				console.log(`The temperature has reached ${this.threshold}`)
-			} else if (this.threshold === 'freezing' && this.alertThrown === false) {
-				this.alertThrown = true
-				console.log(`The temperature has reached ${this.threshold}`)
-			}
-		} else {
-			this.alertThrown = false
+			this.thresholdAlert()
 		}
 	}
 
-	thresholdReachedCheck () {
-		if (this.threshold === "boiling" && this.celcius >= 100) {
-			return true
-		} else if (this.threshold === "freezing" && this.celcius <= 0) {
-			return true
+	thresholdReachedCheck (_temperatureDirection, _previousCelcius) {
+		// If temperature is moving in the same direction as threshold requirement
+		if (_temperatureDirection === this.thresholdDirection) {
+			// High threshold with increasing direction
+			if (this.celcius >= this.thresholdTemperature && _temperatureDirection === 'increase') {
+				if (_previousCelcius > (this.thresholdTemperature - 0.5)) {
+					return false
+				}
+				console.log('inner boil achieved')
+				return true
+			} else if (this.celcius <= this.thresholdTemperature && _temperatureDirection === 'decrease') {
+			// Low threshold with decreasing direction
+				if (_previousCelcius < (this.thresholdTemperature + 0.5)) {
+					return false
+				}
+				console.log('inner freeze achieved')
+				return true
+			}
+
+		// If temperature did not change, no alert
+		} else if (_temperatureDirection === 'no change') {
+			console.log('temperature did not change')
+			return false
+		} else {
+
+		// If temperature goes opposite way of threshold direction
+			console.log('temperature direction opposite of threshold direction')
+			return false
 		}
-		
+	}
+
+	thresholdAlert () {
+		this.alertThrown = true
+		console.log(this.thresholdName + ' achieved')
 	}
 }
 
-let thermostat = new Thermometer(0)
-thermostat.setThreshold('boiling')
-thermostat.setTemperature(1)
-thermostat.setTemperature(100)
-thermostat.setTemperature(99)
-thermostat.setTemperature(101)
+let thermostat = new Thermometer(10)
+// thermostat.setThreshold('boiling', 100, 'increase')
+// thermostat.setTemperature(50)
+// thermostat.setTemperature(100)
+// thermostat.setTemperature(101)
+// console.log(thermostat)
 
-thermostat.setThreshold('freezing')
+thermostat.setThreshold('freezing', 0, 'decrease')
+thermostat.setTemperature(5)
+thermostat.setTemperature(0.3)
 thermostat.setTemperature(0)
-thermostat.setTemperature(10)
-thermostat.setTemperature(-2)
-console.log(thermostat)
+
+console.log(thermostat.readTemperature())
+
+// console.log(thermostat)
