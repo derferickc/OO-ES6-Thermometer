fc7087585dc27c7daf82787e598ddd853e942899
Frederick Choe <frederick.choe@c.wizards.com>
2021-01-14 14:29:38 -0800

    12 test cases for all combinations added but commented out

--- WotC_thermometer.js
+++ WotC_thermometer.js
@@ -47,6 +47,7 @@ class Thermometer {
 		if (this.thresholdTemperatureDirectionMatch(temperatureDirection, nextTemperature, temperatureChange)) {
 			// If returns true, send user the threshold reached alert
 			this.updateTemperature(_celcius)
+			console.log(`The ${this.thresholdName} threshold has been reached!`)
 			return `The ${this.thresholdName} threshold has been reached!`
 		}
 		this.updateTemperature(_celcius)
@@ -92,31 +93,105 @@ class Thermometer {
 	}
 
 	isInsignificantFluctuation (_temperatureDirection) {
+		// Current is inside of the insignificant range below threshold
 		if (_temperatureDirection === 'increase' && this.celcius > this.thresholdTemperature - 0.6) {
 			return true
-		} else if (_temperatureDirection === 'increase' && this.celcius < this.thresholdTemperature + 0.6) {
+		// Current is outside of the insignificant range below threshold
+		} else if (_temperatureDirection === 'increase' && this.celcius < this.thresholdTemperature - 0.6) {
+			return false
+		// Current is inside of the insignificant range above threshold
+		} else if (_temperatureDirection === 'decrease' && this.celcius < this.thresholdTemperature + 0.6) {
 			return true
+		// Current is outside of the insignificant range above threshold
+		} else if (_temperatureDirection === 'decrease' && this.celcius > this.thresholdTemperature + 0.6) {
+			return false
 		}
-		return false
+		// return false
 	}
 }
 
 let thermostat = new Thermometer(10)
+////// Test case 1: Temperature hits boiling threshold (100 °C) once and you expect one alert
 // thermostat.setThreshold('boiling', 100, 'increase', false)
 // thermostat.setTemperature(50)
 // thermostat.setTemperature(100)
-// thermostat.setTemperature(99.4)
-// thermostat.setInsignificantFilter(false)
-// thermostat.setTemperature(99.9)
-// thermostat.setTemperature(102)
+// console.log(thermostat.readTemperature())
 
-// thermostat.setThreshold('freezing', 0, 'decrease')
-// thermostat.setTemperature(5)
+////// Test case 2: Temperature hits boiling threshold (100 °C) twice and you expect two alerts
+// thermostat.setThreshold('boiling', 100, 'increase', false)
+// thermostat.setTemperature(50)
+// thermostat.setTemperature(100)
+// thermostat.setTemperature(99)
+// thermostat.setTemperature(101)
+// console.log(thermostat.readTemperature())
+
+////// Test case 3: Temperature hits boiling threshold (100 °C) twice and you expect one alert because insignificantFilter is toggled to true, however the temperature fluctuation is within the 0.5 °C deemed insignificant
+// thermostat.setThreshold('boiling', 100, 'increase', true)
+// thermostat.setTemperature(100)
+// thermostat.setTemperature(99.5)
+// thermostat.setTemperature(101)
+// console.log(thermostat.readTemperature())
+
+////// Test case 4: Temperature hits boiling threshold (100 °C) twice and you expect two alerts because insignificantFilter is toggled to true, but the temperature fluctuation is outside of the 0.5 °C deemed insignificant
+// thermostat.setThreshold('boiling', 100, 'increase', true)
+// thermostat.setTemperature(100)
+// thermostat.setTemperature(99.4)
+// thermostat.setTemperature(101)
+// console.log(thermostat.readTemperature())
+
+////// Test case 5: Temperature hits boiling threshold (100 °C) twice and you expect two alerts because insignificantFilter is toggled to false, temperature is outside the fluctuations of the 0.5 °C insignificance
+// thermostat.setThreshold('boiling', 100, 'increase', false)
+// thermostat.setTemperature(100)
+// thermostat.setTemperature(99.4)
+// thermostat.setTemperature(101)
+// console.log(thermostat.readTemperature())
+
+////// Test case 6: Temperature hits boiling threshold (100 °C) twice and you expect two alerts because insignificantFilter is toggled to false, temperature is within the temperature fluctuations of 0.5 °C insignificance, but is ignored
+// thermostat.setThreshold('boiling', 100, 'increase', false)
+// thermostat.setTemperature(100)
+// thermostat.setTemperature(99.5)
+// thermostat.setTemperature(101)
+// console.log(thermostat.readTemperature())
+
+////// Test case 7: Temperature hits freezing threshold (0 °C) once and you expect one alert
+// thermostat.setThreshold('freezing', 0, 'decrease', false)
+// thermostat.setTemperature(1)
 // thermostat.setTemperature(0)
-// thermostat.setInsignificantFilter(true)
-// thermostat.setTemperature(0.6)
+// console.log(thermostat.readTemperature())
+
+////// Test case 8: Temperature hits freezing threshold (0 °C) twice and you expect two alerts
+// thermostat.setThreshold('freezing', 0, 'decrease', false)
+// thermostat.setTemperature(0)
+// thermostat.setTemperature(10)
+// thermostat.setTemperature(-1)
+// console.log(thermostat.readTemperature())
+
+////// Test case 9: Temperature hits freezing threshold (0 °C) twice and you expect one alert because insignificantFilter is toggled to true, however the temperature fluctuation is within the 0.5 °C deemed insignificant
+// thermostat.setThreshold('freezing', 0, 'decrease', true)
+// thermostat.setTemperature(0)
 // thermostat.setTemperature(0.5)
 // thermostat.setTemperature(-1)
-
 // console.log(thermostat.readTemperature())
+
+////// Test case 10: Temperature hits freezing threshold (0 °C) twice and you expect two alerts because insignificantFilter is toggled to true, but the temperature fluctuation is outside of the 0.5 °C deemed insignificant
+// thermostat.setThreshold('freezing', 0, 'decrease', true)
+// thermostat.setTemperature(0)
+// thermostat.setTemperature(0.6)
+// thermostat.setTemperature(-1)
+// console.log(thermostat.readTemperature())
+
+////// Test case 11: Temperature hits freezing threshold (0 °C) twice and you expect two alerts because insignificantFilter is toggled to false, temperature is outside the fluctuations of the 0.5 °C insignificance
+// thermostat.setThreshold('freezing', 0, 'decrease', false)
+// thermostat.setTemperature(0)
+// thermostat.setTemperature(0.6)
+// thermostat.setTemperature(-1)
+// console.log(thermostat.readTemperature())
+
+////// Test case 12: Temperature hits freezing threshold (0 °C) twice and you expect two alerts because insignificantFilter is toggled to false, temperature is within the temperature fluctuations of 0.5 °C insignificance, but is ignored
+// thermostat.setThreshold('freezing', 0, 'decrease', false)
+// thermostat.setTemperature(0)
+// thermostat.setTemperature(0.5)
+// thermostat.setTemperature(-1)
+// console.log(thermostat.readTemperature())
+
 // console.log(thermostat)
